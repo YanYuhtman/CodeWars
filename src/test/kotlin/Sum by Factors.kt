38a2f1e0d8b51edc1a4a6b135393abc9ba052e1a
YanYuhtman <yan.yuhtman@gmail.com>
2023-08-20 20:11:26 +0300

    Sum by Factors - wrong approach [exclude the maximum prime logic. it's wrong]

--- src/test/kotlin/Sum by Factors.kt
+++ src/test/kotlin/Sum by Factors.kt
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test
-import java.math.BigInteger
 import kotlin.test.assertEquals
 
 class `Sum by Factors` {
@@ -11,29 +10,64 @@ class `Sum by Factors` {
                 return false
         return true
     }
-    fun findPrimeFactors(n:Int, largesPrime: Int):IntArray {
-        var _n = Math.max(Math.abs(n), largesPrime)
+    fun findPrimeFactors(I:IntArray, largestPrime: Int):IntArray {
         val result = mutableListOf<Int>()
-        while (_n-- > 1)
-            for(i in _n .. 2)
-                if(_n % i == 0 && i.isPrime){
-                    if(largesPrime < 2) return intArrayOf(i)
+        I.forEach {
+            var n = if(largestPrime > 0) largestPrime else Math.abs(it)
+            for (i in n downTo 2) {
+                if (it % i == 0 && i.isPrime) {
+                    if (largestPrime < 2) return intArrayOf(i)
                     result.add(i)
                 }
-        return result.toTypedArray() as IntArray
+            }
+        }
+        return result.distinct().sorted().toIntArray()
     }
 
-    fun getPrimeFactorSums(I:IntArray): List<IntArray>{
-        val maxPrime = findPrimeFactors(Math.max(Math.abs(I.max()),Math.abs(I.min())),-1)[0]
-        val pFactors = findPrimeFactors(I.sumOf { Math.abs(it) },maxPrime)
+    fun sumOfDivided(I: IntArray): String {
+        if(I.isEmpty())
+            return ""
+        val maxPrime = findPrimeFactors(intArrayOf( Math.max(Math.abs(I.max()),Math.abs(I.min()))),-1)[0]
+        return findPrimeFactors(I,maxPrime)
+            .fold(mutableListOf<String>()){acc, i -> acc.add("($i ${I.sumOf {if(it%i == 0) it else 0 }})"); acc}
+            .joinToString("")
 
-        return listOf()
+    }
+
+
+    @Test
+    fun testOne() {
+        val lst = intArrayOf(12, 15)
+        assertEquals("(2 12)(3 27)(5 15)",
+            sumOfDivided(lst))
     }
 
     @Test
-    fun test(){
-        assertEquals(listOf(intArrayOf(2,12), intArrayOf(3,27), intArrayOf(5,15))
-            ,getPrimeFactorSums(intArrayOf(12,15)))
+    fun testTwo() {
+        val lst = intArrayOf(15, 21, 24, 30, 45)
+        assertEquals("(2 54)(3 135)(5 90)(7 21)",
+            sumOfDivided(lst))
+    }
+
+    @Test
+    fun testThree() {
+        val lst = intArrayOf(107, 158, 204, 100, 118, 123, 126, 110, 116, 100)
+        assertEquals("(2 1032)(3 453)(5 310)(7 126)(11 110)(17 204)(29 116)(41 123)(59 118)(79 158)(107 107)",
+            sumOfDivided(lst))
+    }
+
+    @Test
+    fun testFour() {
+        val lst = intArrayOf()
+        assertEquals("",
+            sumOfDivided(lst))
+    }
+
+    @Test
+    fun testFive() {
+        val lst = intArrayOf(1070, 1580, 2040, 1000, 1180, 1230, 1260, 1100, 1160, 1000)
+        assertEquals("(2 12620)(3 4530)(5 12620)(7 1260)(11 1100)(17 2040)(29 1160)(41 1230)(59 1180)(79 1580)(107 1070)",
+            sumOfDivided(lst))
     }
 
 
