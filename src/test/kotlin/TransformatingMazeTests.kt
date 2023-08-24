f9ba1857ed8abafe96c696e9c7b3653aea148a1b
YanYuhtman <yan.yuhtman@gmail.com>
2023-08-24 18:01:19 +0300

    TransformatingMazeTests - almost solved

--- src/test/kotlin/TransformatingMazeTests.kt
+++ src/test/kotlin/TransformatingMazeTests.kt
@@ -11,10 +11,8 @@ internal class  TransformatingMazeTests {
         intArrayOf(-1, 9, 6, 8),
         intArrayOf(12, 7, 7, -2)
     )
-    lateinit var mazes: Array<Array<IntArray>>
-
     enum class Direction(val bitwise: Int) {
-        N(0x8), W(0x4), E(0x1), S(0x2);
+        N(0x8),E(0x1), W(0x4), S(0x2);
         val inverse:Direction
             get() =  when(this){ N -> S; S -> N; W -> E; E -> W}
     }
@@ -37,34 +35,92 @@ internal class  TransformatingMazeTests {
             Direction.E -> (currentX to currentY + 1)
         }
         if ((x < 0 || x > maze.lastIndex || y < 0 || y > maze[maze.lastIndex].lastIndex)
-            || (maze[currentX][currentY] and direction.bitwise != 0)
+            || (maze[currentX][currentY] > 0 && (maze[currentX][currentY] and direction.bitwise != 0))
             || (maze[x][y] < 0 && maze[x][y] != -2)
-            || (maze[x][y] and direction.inverse.bitwise != 0)
+            || (maze[x][y] > 0 && (maze[x][y] and direction.inverse.bitwise != 0))
         ) return null
-        if(maze[x][y] != -2)
-            maze[x][y] = -1
+        maze[currentX][currentY] = -1
         return (x to y)
     }
-    fun findPath(maze: Array<IntArray>, x:Int, y:Int, directions:MutableList<Direction>):List<Direction>?{
-        if(maze[x][y] == -2)
-            return directions
+
+    fun findPath(maze: Array<IntArray>, x:Int, y:Int, rotations:Int, directions:List<String>, results:MutableList<List<String>>){
+        results.maxOfOrNull { it.size }?.let {
+            if(directions.size >= it)
+                return
+        }
+
+        if(maze[x][y] == -2) {
+           results.add(directions)
+           return
+        }
+
 
         Direction.values().forEach {direction->
             makeStep(maze,x,y,direction)?.also {
-                directions.add(direction)
-
-                return findPath(maze.map { it.map { it }.toIntArray() }.toTypedArray(),it.first,it.second,directions.map { it }.toMutableList())
-            }?: return findPath(rotateMaze(maze),x,y,directions.map { it }.toMutableList())
+                val _directions = directions.mapIndexed { index, s -> if(index == directions.lastIndex) s + direction else s }
+                findPath(maze.map { it.map { it }.toIntArray() }.toTypedArray(),it.first,it.second,0,_directions,results)
+            }
         }
-
-        return null
+        if(rotations < 4)
+            findPath(rotateMaze(maze),x,y,rotations + 1, directions + "",results)
+        else
+            return
     }
 
     fun mazeSolver(maze: Array<IntArray>): Array<String>? {
-//        mazes = arrayOf(maze,rotateMaze(maze),rotateMaze(rotateMaze(maze)),rotateMaze(rotateMaze(rotateMaze(maze))))
         val startPoint = getStartPoint(maze)
-        val a = findPath(maze,startPoint.first,startPoint.second, mutableListOf<Direction>())?.joinToString("")
+        var results = mutableListOf<List<String>>()
+        findPath(maze,startPoint.first,startPoint.second, 0, mutableListOf(""),results)
+        return results.sortedBy { it.size }.first().toTypedArray()
+
     }
+
+
+    @Test fun `Example Tests`() {
+        val testCases = arrayOf(
+            Pair(
+                arrayOf(
+                    intArrayOf(4,2,5,4),
+                    intArrayOf(4,15,11,1),
+                    intArrayOf(-1,9,6,8),
+                    intArrayOf(12,7,7,-2)),
+                arrayOf("NNE", "EE", "S", "SS")),
+//            Pair(
+//                arrayOf(
+//                    intArrayOf(6,3,10,4,11),
+//                    intArrayOf(8,10,4,8,5),
+//                    intArrayOf(-1,14,11,3,-2),
+//                    intArrayOf(15,3,4,14,15),
+//                    intArrayOf(14,7,15,5,5)),
+//                arrayOf("", "", "E", "", "E", "NESE")),
+//            Pair(
+//                arrayOf(
+//                    intArrayOf(9,1,9,0,13,0),
+//                    intArrayOf(14,1,11,2,11,4),
+//                    intArrayOf(-1,2,11,0,0,15),
+//                    intArrayOf(4,3,9,6,3,-2)),
+//                arrayOf("E", "SE", "", "E", "E", "E")),
+//            Pair(
+//                arrayOf(
+//                    intArrayOf(-1,6,12,15,11),
+//                    intArrayOf(8,7,15,7,10),
+//                    intArrayOf(13,7,13,15,-2),
+//                    intArrayOf(11,10,8,1,3),
+//                    intArrayOf(12,6,9,14,7)),
+//                null),
+//            Pair(
+//                arrayOf(
+//                    intArrayOf(6,3,0,9,14,13,14),
+//                    intArrayOf(-1,14,9,11,15,14,15),
+//                    intArrayOf(2,15,0,12,6,15,-2),
+//                    intArrayOf(4,10,7,6,15,5,3),
+//                    intArrayOf(7,3,13,13,14,7,0)),
+//                null)
+        )
+
+        testCases.forEach { (maze,sol) -> assertTrue(mazeSolver(maze).contentDeepEquals(sol)) }
+    }
+
     @Test
     fun rotateTest() {
         maze.contentDeepEquals(maze)
@@ -90,19 +146,6 @@ internal class  TransformatingMazeTests {
         )),"The input matrix is unequal to the target (3 rot)")
 
         assertTrue(rotateMaze(rotateMaze(rotateMaze(rotateMaze(maze)))).contentDeepEquals(maze),"The input matrix is unequal to the target (4 rot)")
-    }
-
-    @Test
-    fun mazesCreation() {
-        val maze = TransformingMaze(maze as Array<Array<Any>>)
-        print(mazeToString(maze.getMaze(0)))
-        println()
-        print(mazeToString(maze.getMaze(1)))
-        println()
-        print(mazeToString(maze.getMaze(2)))
-        println()
-        print(mazeToString(maze.getMaze(3)))
-
     }
 
     @Test
