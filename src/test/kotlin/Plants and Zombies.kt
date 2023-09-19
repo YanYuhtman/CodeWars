2e076017099efd9be50f9c8689a4f0f814c0d436
YanYuhtman <yan.yuhtman@gmail.com>
2023-09-19 20:34:34 +0300

    Plants and Zombies - not working

--- src/test/kotlin/Plants and Zombies.kt
+++ src/test/kotlin/Plants and Zombies.kt
@@ -1,13 +1,104 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.math.abs
+import kotlin.text.StringBuilder
 
 class `Plants and Zombies` {
     //https://www.codewars.com/kata/5a5db0f580eba84589000979/kotlin
 
     object PNZ {
+        class Game(val lawn:Array<String>, var zombies: Array<IntArray>){
+
+            fun makeStep(){
+                zombies.forEach {
+                    if(it[0] < 1) {
+                        val r = it[1]
+                        val c = lawn[r].length - 1 + it[0]
+                        lawn[r] = lawn[r].mapIndexed { col, ch -> if (col == c && ch != ' ') 'x' else ch }.joinToString("")
+                    }
+                    it[0] -= 1
+                }
+            }
+            fun checkWinningStatus():Int{
+                if(zombies.any { -it[0] >= lawn[it[1]].length})
+                    return -1
+                if(zombies.isEmpty())
+                    return 1
+                return 0
+            }
+            fun firePonds(){
+                val soldiers = lawn.mapIndexed { r, s-> s.mapNotNull{p-> if (p.isDigit()) (r to p.digitToInt()) else null } }.flatten()
+                soldiers.forEach { soldier->
+                    for(i in (1 .. soldier.second))
+                        zombies.filter { it[0] < 0 && it[1] == soldier.first }.minByOrNull { it[0] }
+                            ?.let { it[2]-=1; if(it[2] <= 0) zombies = zombies.toMutableList().apply { this.remove(it) }.toTypedArray() }
+                            ?: break
+                }
+            }
+            fun fireASes() {
+                val soldiers =
+                    lawn.mapIndexed { r, s -> s.mapIndexedNotNull { c, S -> if (S == 'S') (r to c) else null } }
+                        .flatten()
+                        .sortedWith(compareByDescending <Pair<Int, Int>> { it.second }.thenBy { it.first })
+                soldiers.forEach { S ->
+                    val targets = zombies.mapIndexedNotNull { i, z ->
+                        var out: Pair<IntArray, Int>? = null
+                        val dr = z[1] - S.first
+                        val dc = lawn[z[1]].length - 1 + z[0] - S.second
+                        if (z[0] < 0 && dc > 0) {
+                            val angle = if (dr == 0) 0.0 else dr / dc.toDouble()
+                            if (abs(angle) == 1.0 || angle == 0.0)
+                                out = (z to dr * dr + dc * dc)
+
+                        }
+                        out
+                    }.sortedBy { it.second }.distinctBy { it.second }
+
+                    targets.forEach {
+                        it.first[2] -= 1
+                        if (it.first[2] <= 0)
+                            zombies = zombies.toMutableList().apply { this.remove(it.first) }.toTypedArray()
+                    }
+
+                }
+            }
+
+            fun play():Int?{
+                var moveNumber = 1
+                var status = 0
+
+                while (status == 0) {
+                    println(this)
+                    makeStep()
+                    firePonds()
+                    fireASes()
+                    moveNumber += 1
+                    status = checkWinningStatus()
+                }
+                return when (status){
+                    -1 -> moveNumber
+                    else -> null
+                }
+            }
+
+            override fun toString(): String {
+                val zombies = zombies.filter { it[0] <= 0 }.associateBy { (it[1] to lawn[0].length - 1 + it[0]) }
+                val out = StringBuilder((0 until lawn[0].length).toList().toString()).append("\n")
+                return lawn.foldIndexed(out) { r, S, s->
+                    s.mapIndexed{ c, ch->
+                        val per = if(c == 0) 2 else 3
+                        val _ch = zombies[(r to c)]?.let { 'Z' } ?: ch
+                        S.append("%${per}c".format(_ch))
+                    }
+                    S.append("\n");
+                }.toString()
+
+            }
+
+
+        }
         fun plantsAndZombies(lawn:Array<String>,zombies:Array<IntArray>): Int? {
-            // your code goes here. you can do it!
-            return null
+            return Game(lawn,zombies).play()
         }
     }
 
@@ -17,97 +108,97 @@ class `Plants and Zombies` {
     private val exampleSols = listOf(10,12,20,19,null)
 
     private val exampleTests = listOf(
-        Pair(
-            arrayOf(
-                "2       ",
-                "  S     ",
-                "21  S   ",
-                "13      ",
-                "2 3     "),
-            arrayOf(
-                intArrayOf(0,4,28),
-                intArrayOf(1,1,6),
-                intArrayOf(2,0,10),
-                intArrayOf(2,4,15),
-                intArrayOf(3,2,16),
-                intArrayOf(3,3,13))),
-        Pair(
-            arrayOf(
-                "11      ",
-                " 2S     ",
-                "11S     ",
-                "3       ",
-                "13      "),
-            arrayOf(
-                intArrayOf(0,3,16),
-                intArrayOf(2,2,15),
-                intArrayOf(2,1,16),
-                intArrayOf(4,4,30),
-                intArrayOf(4,2,12),
-                intArrayOf(5,0,14),
-                intArrayOf(7,3,16),
-                intArrayOf(7,0,13))),
-        Pair(
-            arrayOf(
-                "12        ",
-                "3S        ",
-                "2S        ",
-                "1S        ",
-                "2         ",
-                "3         "),
-            arrayOf(
-                intArrayOf(0,0,18),
-                intArrayOf(2,3,12),
-                intArrayOf(2,5,25),
-                intArrayOf(4,2,21),
-                intArrayOf(6,1,35),
-                intArrayOf(6,4,9),
-                intArrayOf(8,0,22),
-                intArrayOf(8,1,8),
-                intArrayOf(8,2,17),
-                intArrayOf(10,3,18),
-                intArrayOf(11,0,15),
-                intArrayOf(12,4,21))),
-        Pair(
-            arrayOf(
-                "12      ",
-                "2S      ",
-                "1S      ",
-                "2S      ",
-                "3       "),
-            arrayOf(
-                intArrayOf(0,0,15),
-                intArrayOf(1,1,18),
-                intArrayOf(2,2,14),
-                intArrayOf(3,3,15),
-                intArrayOf(4,4,13),
-                intArrayOf(5,0,12),
-                intArrayOf(6,1,19),
-                intArrayOf(7,2,11),
-                intArrayOf(8,3,17),
-                intArrayOf(9,4,18),
-                intArrayOf(10,0,15),
-                intArrayOf(11,4,14))),
-        Pair(
-            arrayOf(
-                "1         ",
-                "SS        ",
-                "SSS       ",
-                "SSS       ",
-                "SS        ",
-                "1         "),
-            arrayOf(
-                intArrayOf(0,2,16),
-                intArrayOf(1,3,19),
-                intArrayOf(2,0,18),
-                intArrayOf(4,2,21),
-                intArrayOf(6,3,20),
-                intArrayOf(7,5,17),
-                intArrayOf(8,1,21),
-                intArrayOf(8,2,11),
-                intArrayOf(9,0,10),
-                intArrayOf(11,4,23),
-                intArrayOf(12,1,15),
-                intArrayOf(13,3,22)))
-    )
+//        Pair(
+//            arrayOf(
+//                "2       ",
+//                "  S     ",
+//                "21  S   ",
+//                "13      ",
+//                "2 3     "),
+//            arrayOf(
+//                intArrayOf(0,4,28),
+//                intArrayOf(1,1,6),
+//                intArrayOf(2,0,10),
+//                intArrayOf(2,4,15),
+//                intArrayOf(3,2,16),
+//                intArrayOf(3,3,13))),
+//        Pair(
+//            arrayOf(
+//                "11      ",
+//                " 2S     ",
+//                "11S     ",
+//                "3       ",
+//                "13      "),
+//            arrayOf(
+//                intArrayOf(0,3,16),
+//                intArrayOf(2,2,15),
+//                intArrayOf(2,1,16),
+//                intArrayOf(4,4,30),
+//                intArrayOf(4,2,12),
+//                intArrayOf(5,0,14),
+//                intArrayOf(7,3,16),
+//                intArrayOf(7,0,13))),
+//        Pair(
+//            arrayOf(
+//                "12        ",
+//                "3S        ",
+//                "2S        ",
+//                "1S        ",
+//                "2         ",
+//                "3         "),
+//            arrayOf(
+//                intArrayOf(0,0,18),
+//                intArrayOf(2,3,12),
+//                intArrayOf(2,5,25),
+//                intArrayOf(4,2,21),
+//                intArrayOf(6,1,35),
+//                intArrayOf(6,4,9),
+//                intArrayOf(8,0,22),
+//                intArrayOf(8,1,8),
+//                intArrayOf(8,2,17),
+//                intArrayOf(10,3,18),
+//                intArrayOf(11,0,15),
+//                intArrayOf(12,4,21))),
+//        Pair(
+//            arrayOf(
+//                "12      ",
+//                "2S      ",
+//                "1S      ",
+//                "2S      ",
+//                "3       "),
+//            arrayOf(
+//                intArrayOf(0,0,15),
+//                intArrayOf(1,1,18),
+//                intArrayOf(2,2,14),
+//                intArrayOf(3,3,15),
+//                intArrayOf(4,4,13),
+//                intArrayOf(5,0,12),
+//                intArrayOf(6,1,19),
+//                intArrayOf(7,2,11),
+//                intArrayOf(8,3,17),
+//                intArrayOf(9,4,18),
+//                intArrayOf(10,0,15),
+//                intArrayOf(11,4,14))),
+//        Pair(
+//            arrayOf(
+//                "1         ",
+//                "SS        ",
+//                "SSS       ",
+//                "SSS       ",
+//                "SS        ",
+//                "1         "),
+//            arrayOf(
+//                intArrayOf(0,2,16),
+//                intArrayOf(1,3,19),
+//                intArrayOf(2,0,18),
+//                intArrayOf(4,2,21),
+//                intArrayOf(6,3,20),
+//                intArrayOf(7,5,17),
+//                intArrayOf(8,1,21),
+//                intArrayOf(8,2,11),
+//                intArrayOf(9,0,10),
+//                intArrayOf(11,4,23),
+//                intArrayOf(12,1,15),
+//                intArrayOf(13,3,22)))
+//    )
 }
